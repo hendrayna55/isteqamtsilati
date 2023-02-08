6335425c5b64bb83d1afe2ee189ad3b3d164fe15
isteqamtsilati <hendrayna55@gmail.com>
2023-02-08 14:50:21 +0700

    Upload File

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\landing\HomeController;
+use App\Http\Controllers\dashboard\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/home',[HomeController::class,'index']);
